e77eb3aea111dfdfe0511a42335ed2d1f1127d06
tosin <tosin.sonuyi@gmail.com>
2019-01-25 21:29:22 -0500

    adds script for commenting out tweepy line for when tweeting every commit gets annoying asfck

--- stop_tweeting.sh
+++ stop_tweeting.sh
@@ -2,17 +2,14 @@
 
 # This is a program that stops your repository from tweeting commits!
 
-echo "Hello, "$USER".  This script will stop tweeting out your commits for "$REPOSITORY""
+echo -n "Enter the Path/to/your/repository (autocomplete enabled & must end in slash) and press [ENTER]: "
+read -e REPOSITORY_PATH
 
-echo -n "Enter your Twitter consumer key and press [ENTER]: "
-read twitter_consumer_key
-echo -n "Enter your Twitter consumer secret and press [ENTER]: "
-read twitter_consumer_secret
+# tilde expansion for the case when it exists 
+# https://stackoverflow.com/questions/3963716/how-to-manually-expand-a-special-variable-ex-tilde-in-bash
+REPOSITORY_PATH="${REPOSITORY_PATH/#\~/$HOME}"
 
-echo -n "Enter your Twitter access token and press [ENTER]: "
-read twitter_access_token
-
-echo -n "Enter your Twitter access token secret and press [ENTER]: "
-read twitter_access_token_secret
-
-echo
+# the line that needs to be commented out to stop tweeting
+TWEETING_LINE="status = api.update_status(status=tweet_str)"
+echo "commenting out the line that does the tweeting"
+sed -i -e "s|${TWEETING_LINE}|#${TWEETING_LINE}|g" "${REPOSITORY_PATH}.git/hooks/post_commit_hook_autogen.py"
